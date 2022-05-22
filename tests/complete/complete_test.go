5f978d64fb3b853c05048cb524614fbf9f60acef
pipo <pipo@flufi.io>
2022-05-22 14:50:36 -0300

    updated template files

--- tests/complete/complete_test.go
+++ tests/complete/complete_test.go
@@ -1,18 +1,47 @@
 package test
 
 import (
-	"testing"
-
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strconv"
+	"testing"
+	"time"
 )
 
 func TestCompleteExample(t *testing.T) {
-	// retryable errors in terraform testing.
+	godotenv.Load("../../.env")
+
+	// DELAY is the time in seconds to run terraform destroy after terraform apply
+	DELAY, _ := strconv.Atoi(os.Getenv("DELAY"))
+	varFiles := []string{"../../examples/complete/terraform.tfvars"}
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../../examples/complete",
+		VarFiles:     varFiles,
+		Vars:         map[string]interface{}{},
+		Upgrade:      true,
+		Reconfigure:  true,
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
+	// Delay the execution of the terraform destroy
+	defer func() {
+		delay(DELAY)
+	}()
 
 	terraform.InitAndApply(t, terraformOptions)
+
+}
+
+func delay(seconds int) {
+	for {
+		if seconds <= 0 {
+			break
+		} else {
+			log.Println(seconds)
+			time.Sleep(1 * time.Second)
+			seconds--
+		}
+	}
 }
