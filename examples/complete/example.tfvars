bd630d816c44db6b4e38350ea51a7ee277243f33
Pipo <mnsanfilippo@gmail.com>
2023-03-26 10:05:30 -0300

    testing with variables

--- examples/complete/example.tfvars
+++ examples/complete/example.tfvars
@@ -1,2 +1,5 @@
-environment = "terratest"
-name        = "terraform-module-template"
+environment      = "terratest"
+name             = "terraform-module-template"
+special          = "!#$%&*()-_=+[]{}<>:?"
+override_special = true
+length           = 35
--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,5 +1,7 @@
 module "random" {
-  source  = "../../"
-  length  = var.length
-  context = module.this.context
+  source           = "../../"
+  length           = var.length
+  context          = module.this.context
+  override_special = var.override_special
+  special          = var.special
 }
