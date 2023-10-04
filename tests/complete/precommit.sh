2581fd2e71e0a2b836ec6d07a77b9a0f9ea4f524
Pipo Sanfilippo <45714073+mnsanfilippo@users.noreply.github.com>
2023-10-03 21:16:27 -0300

    checkov (#17)
    * checkov
    
    * 1password
    
    * terratest fix
    
    * terratest fix
    
    * updated 1password to json
    
    * updated 1password to json
    
    * updated 1password to json
    
    * updated 1password to json
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * pre-commit
    
    * removed infracost.yml

--- tests/complete/precommit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd tests/complete || exit
-rm -rf go.*
-export GOOS=linux GOARCH=arm64 CGO_ENABLED=0
-go mod init test
-go mod tidy >> /dev/null
-go test -count=1 -timeout 120m -v ./...
-exit $?
