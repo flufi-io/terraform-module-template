7f3be4b384b12827823c898e4462134cde528c7d
Pipo Sanfilippo <45714073+mnsanfilippo@users.noreply.github.com>
2024-01-13 18:08:30 -0300

    s3 backend  (#21)
    * reordered-files
    
    * reordered-files

--- tests/complete/complete_test.go
+++ tests/complete/complete_test.go
@@ -2,21 +2,47 @@ package test
 
 import (
 	"fmt"
-	"github.com/gruntwork-io/terratest/modules/random"
-	"github.com/gruntwork-io/terratest/modules/terraform"
 	"os"
 	"os/exec"
 	"strconv"
 	"testing"
 	"time"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	log "github.com/sirupsen/logrus"
 )
 
 var TimeToDestroy, _ = strconv.Atoi(os.Getenv("TIME_TO_DESTROY"))
 
-func Test(t *testing.T) {
+func TestMain(m *testing.M) {
+	// Decrypt secrets before running tests
+	err := runSecretsScript("secrets.sh", "-d", "sandbox")
+	if err != nil {
+		log.Fatalf("Failed to decrypt secrets: %v", err)
+	}
+	// Run pre-commit due to the decrypted secrets file.
+	err = runInitCommands()
+	if err != nil {
+		log.Fatalf("Failed to run commands: %v", err)
+	}
+	_ = runPreCommit()
+
+	// Run tests
+	exitVal := m.Run()
+
+	// Encrypt secrets after tests
+	err = runSecretsScript("secrets.sh", "-e", "sandbox")
+	if err != nil {
+		log.Fatalf("Failed to encrypt secrets: %v", err)
+	}
+
+	// Exit with the exit code determined by the tests
+	os.Exit(exitVal)
+}
+
+func TestTerraform(t *testing.T) {
 	t.Parallel()
-	// Run secrets.sh script
-	runSecretsScript(t, "secrets.sh", "-d", "sandbox")
 	// Generate a random string
 	randHash := random.UniqueId()
 	originalName := terraform.GetVariableAsStringFromVarFile(t, "../../examples/complete/fixtures.sandbox.tfvars.json", "name")
@@ -28,13 +54,20 @@ func Test(t *testing.T) {
 		Vars: map[string]interface{}{
 			"name": name,
 		},
-		Upgrade:              true,
-		Reconfigure:          true,
+		Upgrade:              true, // already done in init commands
+		Reconfigure:          true, // already done in init commands
 		Lock:                 true,
 		SetVarsAfterVarFiles: true,
+		BackendConfig: map[string]interface{}{
+			"bucket":         "flufi-terraform-backend-sandbox-us-east-1",
+			"key":            originalName + ".tfstate",
+			"region":         "us-east-1",
+			"dynamodb_table": "flufi-terraform-backend-sandbox-us-east-1",
+			"encrypt":        true,
+			"acl":            "bucket-owner-full-control",
+		},
 	})
 
-	defer runSecretsScript(t, "secrets.sh", "-e", "sandbox")
 	defer terraform.Destroy(t, terraformOptions)
 	defer func() {
 		timer(TimeToDestroy)
@@ -42,18 +75,74 @@ func Test(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 }
+func TestPreCommit(t *testing.T) {
+	err := runPreCommit()
+	if err != nil {
+		t.Fatalf("Failed to run commands: %s", err)
+	}
+}
 
-func runSecretsScript(t *testing.T, scriptName string, args ...string) {
+func runSecretsScript(scriptName string, args ...string) error {
 	// Prepend the script name to the args slice
-	t.Log("Running script: " + scriptName)
+	log.Printf("Running script: %s", scriptName)
 	commandArgs := append([]string{scriptName}, args...)
 
 	cmd := exec.Command("bash", commandArgs...)
 	cmd.Dir = "../../examples/complete/"
 	err := cmd.Run()
 	if err != nil {
-		t.Fatalf("Failed to execute script: %s", err)
+		log.Printf("Failed to execute script: %s", err)
+		return err
 	}
+	return nil
+}
+
+func runInitCommands() error {
+	// Run 'tfenv use latest'
+	err := runCommand("tfenv", "use", "latest")
+	if err != nil {
+		log.Printf("Failed to run tfenv use latest: %s", err)
+		return err
+	}
+	// Run 'pre-commit install --install-hooks'
+	err = runCommand("pre-commit", "install", "--install-hooks")
+	if err != nil {
+		log.Printf("Failed to run pre-commit install --install-hooks: %s", err)
+	}
+
+	// Run 'pre-commit autoupdate'
+	err = runCommand("pre-commit", "autoupdate")
+	if err != nil {
+		log.Printf("Failed to run pre-commit autoupdate: %s", err)
+	}
+
+	// Run 'pre-commit run -a'
+	err = runCommand("pre-commit", "run", "-a")
+	if err != nil {
+		log.Printf("Failed to run pre-commit run -a: %s", err)
+	}
+	return nil
+}
+
+func runPreCommit() error {
+	// Run 'pre-commit run -a'
+	if err := runCommand("pre-commit", "run", "-a"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func runCommand(command string, args ...string) error {
+	fmt.Printf("Running command: %s %v\n", command, args)
+	cmd := exec.Command(command, args...)
+	cmdOutput, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Error executing command: %s\n", err)
+		fmt.Printf("Command output: %s\n", string(cmdOutput))
+		return err
+	}
+	fmt.Printf("Command output: %s\n", string(cmdOutput))
+	return nil
 }
 
 func timer(s int) {
